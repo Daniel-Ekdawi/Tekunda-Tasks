5f308088617e782612d2813ed85e629a69256963
Daniel-Ekdawi <d.ekdawi@tekunda.com>
2025-07-02 14:57:35 +0300

    Interface for super admin to manage users

---
+++ fullstack-demo/frontend/src/app/users/page.jsx
@@ -0,0 +1,9 @@
+import ManageAllUsersComponent from "@/components/superAdmin/ManageAllUsers"
+
+const UsersPage = () => {
+    return <div className="mt-[5%]">
+        <div className="px-[2%]"><ManageAllUsersComponent /></div>
+    </div>
+}
+
+export default UsersPage
