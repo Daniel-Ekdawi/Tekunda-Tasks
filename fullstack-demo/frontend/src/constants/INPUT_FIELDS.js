61410daecc8dc3d097a6f90776bc72a9c5003b9b
Daniel-Ekdawi <d.ekdawi@tekunda.com>
2025-07-10 17:07:07 +0300

    Added hotel and room input fields

--- fullstack-demo/frontend/src/constants/INPUT_FIELDS.js
+++ fullstack-demo/frontend/src/constants/INPUT_FIELDS.js
@@ -81,5 +81,101 @@ const SIGNUP_FIELDS = [
     },
 ]
 
+const HOTEL_FIELDS = [
+    {
+        title: "Name",
+        required: true,
+        type: 'text',
+        tag: 'input',
+        
+    },
+    {
+        title: "Phone Number",
+        required: true,
+        type: 'tel',
+        tag: 'input',
+        pattern: '^\+?[0-9]{10,15}$',
+        
+    },
+    {
+        title: "Email",
+        required: true,
+        type: 'email',
+        tag: 'input',
+        
+    },
+    {
+        title: "Swimming Pools",
+        required: true,
+        type: 'number',
+        tag: 'input',
+        
+    },
+    {
+        title: "Max Reservations",
+        required: true,
+        type: 'number',
+        tag: 'input',
+        
+    },
+    {
+        title: "Gym",
+        type: 'checkbox',
+        tag: 'input',
+        classes: checkboxClasses,
+        
+    },
+    {
+        title: "Spa",
+        type: 'checkbox',
+        tag: 'input',
+        classes: checkboxClasses,
+        
+    },
+    {
+        title: "WiFi",
+        type: 'checkbox',
+        tag: 'input',
+        classes: checkboxClasses,
+        
+    },
+    {
+        title: "Parking",
+        type: 'checkbox',
+        tag: 'input',
+        classes: checkboxClasses,
+        
+    },
+]
+const ROOM_FIELDS = [
+    {
+        title: "Number",
+        required: true,
+        type: 'text',
+        tag: 'input',
+    },
+    {
+        title: "Price",
+        required: true,
+        type: 'number',
+        tag: 'input',
+    },
+    {
+        title: "Description",
+        required: true,
+        type: 'text',
+        tag: 'input',
+    },
+    {
+        title: "Type",
+        required: true,
+        tag: 'select',
+        options: [
+            { value: 'single', title: 'Single' },
+            { value: 'double', title: 'Double' },
+        ],
+    },
+]
+
 export { LOGIN_FIELDS, SIGNUP_FIELDS, HOTEL_FIELDS, ROOM_FIELDS }
 // Strategy Behavioral Pattern
