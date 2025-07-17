d5888a091efb8a40a02ce72a962fbc4a1771c686
Daniel-Ekdawi <d.ekdawi@tekunda.com>
2025-07-17 15:29:42 +0300

    Modified Generic Button class

--- fullstack-demo/frontend/src/components/buttons/GenericButton.jsx
+++ fullstack-demo/frontend/src/components/buttons/GenericButton.jsx
@@ -1,6 +1,6 @@
-const GenericButton = ({ text, colorClasses, onClick, disabled }) => {
+const GenericButton = ({ text, colorClasses, onClick, disabled, extraClasses }) => {
     return <button
-        className={`${colorClasses} transition-all hover:cursor-pointer shadow-2xl rounded-xl px-3 py-1 w-full`}
+        className={`${colorClasses} transition-all hover:cursor-pointer shadow-2xl rounded-xl px-1 md:px-2 lg:px-3 py-1 w-full ${extraClasses}`}
         onClick={onClick}
         disabled={disabled}
     >{text}</button>
